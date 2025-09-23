d5e4c44f10b89d75ef91f4ffad5c93e7c0d49361
Andrej Bočkaj <106064818+BockajAndrej@users.noreply.github.com>
2025-09-23 19:40:55 +0200

    feat: Initial commit

--- FlashCards.Web.App/Components/Matrixes/MatrixCardItem.razor.cs
+++
@@ -1,9 +0,0 @@
-using FlashCards.Web.Bl.ApiClient;
-using Microsoft.AspNetCore.Components;
-
-namespace FlashCards.Web.App.Components.Matrixes;
-
-public partial class MatrixCardItem : ComponentBase
-{
-	[Parameter] public CardListModel CardList { get; set; } = null!;
-}
